71c339f05f85e46937a95ae222cf9195bddfe9f9
DevDaisy1 <123300530+DevDaisy1@users.noreply.github.com>
2025-06-27 13:23:17 +0700

    увкангшщз

--- src/shared/lib/api/transaction/index.ts
+++ src/shared/lib/api/transaction/index.ts
@@ -5,40 +5,40 @@ import { Transaction } from "./data"
 
 const token = sessionStorage.getItem('access')
 
-console.log(`ws://185.103.70.190:8080/api/ws?token=${sessionStorage.getItem('access')}`)
+console.log(`wss://185.103.70.190:8080/api/ws?token=${sessionStorage.getItem('access')}`)
 
 const transactionAdapter = createEntityAdapter<Transaction>({})
-const ws = new WebSocket(`ws://185.103.70.190:8080/api/ws?token=${sessionStorage.getItem('access')}`) 
+const ws = new WebSocket(`wss://185.103.70.190:8080/api/ws?token=${sessionStorage.getItem('access')}`)
 ws.onopen = e => console.log("открылись", e)
-ws.onclose = (e) => console.log("закрылись",e)
-ws.onerror = e => console.log("ошибка",e)
+ws.onclose = (e) => console.log("закрылись", e)
+ws.onerror = e => console.log("ошибка", e)
 const transactions = injectToApi({
-    endpoints: builder=>({
+    endpoints: builder => ({
         getAllTransactions: builder.query<EntityState<Transaction, string>, Bill["id"]>({
-            query: (id)=> ({
+            query: (id) => ({
                 url: `/api/core/bill/transactions/${id}`,
-                method:"GET",
+                method: "GET",
                 headers: {
-                    Authorization: `Bearer ${token}` 
-                  }
+                    Authorization: `Bearer ${token}`
+                }
             }),
             transformResponse(response: Array<Transaction>) {
-                return transactionAdapter.addMany(transactionAdapter.getInitialState(),response)
+                return transactionAdapter.addMany(transactionAdapter.getInitialState(), response)
             },
-            providesTags: result => 
+            providesTags: result =>
                 result ? [...(result.ids as string[]).map(id => ({ type: 'TRANSACTION' as const, id })), 'TRANSACTION'] : ['TRANSACTION'],
-            async onCacheEntryAdded(_, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }){
-                
-                
-                try{
+            async onCacheEntryAdded(_, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }) {
+
+
+                try {
                     await cacheDataLoaded
                     const listner = (event: MessageEvent<any>) => {
                         const data = JSON.parse(event.data) as Transaction
                         console.log(data)
-                        if(data){
+                        if (data) {
                             updateCachedData(draft => {
                                 transactionAdapter.upsertOne(draft, {
-                                    id: data.id, 
+                                    id: data.id,
                                     from: data.from,
                                     to: data.to,
                                     amount: data.amount
@@ -48,7 +48,7 @@ const transactions = injectToApi({
                     }
                     ws.onmessage = listner
                 }
-                catch {}
+                catch { }
                 await cacheEntryRemoved
                 console.log("закрываемся")
                 ws.close()
@@ -57,5 +57,5 @@ const transactions = injectToApi({
     })
 })
 
-export const {useGetAllTransactionsQuery}=transactions
+export const { useGetAllTransactionsQuery } = transactions
 export type { Transaction } from './data'
